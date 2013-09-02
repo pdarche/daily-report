4103cbafe18d3d7b54e7c18fd33625d264db053c
Peter Darche <pdarche@gmail.com>
2013-09-01 21:16:09 -0400

    Add foursquare template.  Add checkin venue name data to email

--- report.py
+++ report.py
@@ -2,10 +2,13 @@ from settings import *
 
 # service wrapper imports 
 import fitbit
+import foursquare
 
 # stdlib imports 
 import json
 from datetime import date
+import datetime
+import time
 
 #template imports
 from jinja2 import Template
@@ -37,9 +40,9 @@ class ReportGenerator():
 
 		self.service_data = {
 			"fitbit" : self.add_fitbit,
-			"withings" : None,
+			"withings" : self.add_withings,
 			"open_paths" : None,
-			"foursquare" : None,
+			"foursquare" : self.add_foursquare,
 			"flickr" : None	
 		}
 		# if service is in services.keys()
@@ -50,6 +53,7 @@ class ReportGenerator():
 
 		# self.services = dict(self.services.items() + {'name' : self.name}.items())
 		self.send_mail()
+		# self.add_foursquare()
 
 	def add_fitbit(self):
 		f = fitbit.FitBit()
@@ -71,7 +75,22 @@ class ReportGenerator():
 		return fitbit_data
 
 	def add_withings(self):
-		
+		# stuff to add here
+		pass
+
+	def add_foursquare(self):
+		client = foursquare.Foursquare(client_id=FOURSQUARE_CLIENT_ID, client_secret=FOURSQUARE_CLIENT_SECRET, redirect_uri='http://127.0.0.1:8000')
+		client.set_access_token(FOURSQUARE_ACCESS_TOKEN)
+		today_string = date.today().strftime("%m/%d/%Y")
+		today = int(time.mktime(datetime.datetime.strptime(today_string, "%m/%d/%Y").timetuple()))
+		checkins = client.users.checkins(params={"afterTimestamp": today})["checkins"]["items"]
+		for checkin in checkins:
+			lat = checkin["venue"]["location"]["lat"]
+			lnf = checkin["venue"]["location"]["lng"]
+			name = checkin["venue"]["name"]
+
+		print checkins
+		return { "checkins":checkins }
 
 	def send_mail(self):
 		EMAIL_TO = "pdarche@gmail.com"
@@ -105,7 +124,7 @@ class ReportGenerator():
 
 		return tmpl.render(self.services)
 
-report = ReportGenerator('Peter Darche', ['fitbit'])
+report = ReportGenerator('Peter Darche', ['fitbit', 'foursquare'])
 
 
 
